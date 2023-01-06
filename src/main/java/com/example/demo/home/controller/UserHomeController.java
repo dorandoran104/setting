608f262aa5ade0958ef4ec239b11a4a7dfe42893
dorandoran104 <pgh_96@naver.com>
2023-01-06 23:55:57 +0900

    setting

--- src/main/java/com/example/demo/home/controller/UserHomeController.java
+++ src/main/java/com/example/demo/home/controller/UserHomeController.java
@@ -1,11 +1,11 @@
-package com.example.demo.article.controller;
+package com.example.demo.home.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class UsrArticleController {
+public class UserHomeController {
 	@RequestMapping("/user/home/main")
 	@ResponseBody
 	public String showMain() {
